ce2901b65a47caaf2f0248ad26e6a079fea88617
Arnav Gupta <guptaarn@msu.edu>
2025-03-23 18:50:21 -0400

    introduced basic gemini and markdown results

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
-import apiKey from "./hidden";
+import apiKeys from "./hidden.js";
+const mistralApiKey = apiKeys.mistralApiKey;
+const geminiApiKey = apiKeys.geminiApiKey;
 document.getElementById('myFile').addEventListener('change', async function() {
     /// get fileUploaded, returns file object at index 0
     let fileUploaded = this.files.item(0);
@@ -12,7 +14,7 @@ document.getElementById('myFile').addEventListener('change', async function() {
     const uploaded_pdf = await fetch('https://api.mistral.ai/v1/files', {
         method: 'POST',
         headers: {
-            "Authorization": `Bearer ${apiKey}`
+            "Authorization": `Bearer ${mistralApiKey}`
         },
         body: form,
     })
@@ -23,17 +25,17 @@ document.getElementById('myFile').addEventListener('change', async function() {
         method: 'GET',
         headers: {
             "Accept": "application/json" ,
-            "Authorization": `Bearer ${apiKey}`
+            "Authorization": `Bearer ${mistralApiKey}`
         },
     })
     const responseJSON = await signedUrl.json();
-    console.log("here",responseJSON)
+    console.log(responseJSON)
     /// communicate with model and turn pdf to md
     const ocrResponse = await fetch(`https://api.mistral.ai/v1/ocr`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json" ,
-            "Authorization": `Bearer ${apiKey}`
+            "Authorization": `Bearer ${mistralApiKey}`
         },
         body: JSON.stringify({
             "model": "mistral-ocr-latest",
@@ -44,8 +46,28 @@ document.getElementById('myFile').addEventListener('change', async function() {
             "include_image_base64": true
         }),
     })
-    const pages = await ocrResponse.json();
-    console.log(pages)
+    const ocrJson = await ocrResponse.json();
+    let markdownExport= ""
+    const eachPage = ocrJson.pages
+    for(let i = 0; i < eachPage.length; i++){
+        markdownExport += eachPage[i].markdown + " ";
+    }
+    console.log(markdownExport)
+    const geminiRequestResponse = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${geminiApiKey}`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify({
+            "contents": [{
+                "parts": [{"text": "Explain how AI works in 5 words"}]
+            }]
+        })
+    })
+    const geminiJson = await geminiRequestResponse.json()
+    console.log(geminiJson)
+    let geminiResponse = geminiJson.candidates[0].content.parts[0].text;
+    console.log(geminiResponse)
 })
 
 
